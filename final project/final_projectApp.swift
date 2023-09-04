cd2c0690ff790b9a965ec50f89ea1b17229f8ae6
Govindarajan Nikhilesh <nikhilesh.govind@gmail.com>
2023-09-04 09:57:39 +0800

    Initial Commit

---
+++ final project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final project
+//
+//  Created by T Krobot on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
